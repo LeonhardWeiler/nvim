b9178f03c78c8f65b0a83cb1c513e75e40f0a269
Leonhard Weiler <Laser_Leo@hotmail.com>
2024-04-27 00:00:11 +0200

    auto pairs fixed and live server added

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -2,5 +2,6 @@ require('nvim-treesitter.configs').setup {
     ensure_installed = { "vimdoc", "javascript", "typescript", "c", "lua", "rust", "css", "html" },
     auto_install = true,
     hightlight = { enable = true },
-    indent = { enable = true }
+    indent = { enable = true },
+    autopairs = { enable = true },
 }
--- lua/leonhard/packer.lua
+++ lua/leonhard/packer.lua
@@ -1,51 +1,61 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.5',
-        requires = { {'nvim-lua/plenary.nvim'} }
+  use 'wbthomason/packer.nvim'
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use { "Shatur/neovim-ayu", as = "ayu" }
+  use({
+    "folke/trouble.nvim",
+    config = function()
+      require("trouble").setup {
+        icons = true,
+      }
+    end
+  })
+
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/playground')
+  use('theprimeagen/harpoon')
+  use('mattn/emmet-vim')
+  use {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+  }
+  use('prettier/vim-prettier')
+  use({
+    "aurum77/live-server.nvim",
+    run = function()
+      require"live_server.util".install()
+    end,
+    cmd = { "LiveServer", "LiveServerStart", "LiveServerStop" },
+  })
+  use('github/copilot.vim')
+  use('mbbill/undotree')
+  use('tpope/vim-fugitive')
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
+
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},
+      {'rafamadriz/friendly-snippets'},
     }
-    use { "Shatur/neovim-ayu", as = "ayu" }
-    use({
-        "folke/trouble.nvim",
-        config = function()
-            require("trouble").setup {
-                icons = true,
-            }
-        end
-    })
-
-    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-    use('nvim-treesitter/playground')
-    use('theprimeagen/harpoon')
-    use('mattn/emmet-vim')
-    use('jiangmiao/auto-pairs')
-    use('prettier/vim-prettier')
-    use('github/copilot.vim')
-    use('mbbill/undotree')
-    use('tpope/vim-fugitive')
-     use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v1.x',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
-
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
-		  {'saadparwaiz1/cmp_luasnip'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'hrsh7th/cmp-nvim-lua'},
-
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},
-		  {'rafamadriz/friendly-snippets'},
-	  }
   }
   use("folke/zen-mode.nvim")
 end)
