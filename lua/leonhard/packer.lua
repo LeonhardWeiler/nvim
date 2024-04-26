c99917b026260b03d8aa7574847536d6f6587e5a
Leonhard Weiler <Laser_Leo@hotmail.com>
2024-04-26 21:07:27 +0200

    Copilot added

--- lua/leonhard/packer.lua
+++ lua/leonhard/packer.lua
@@ -18,9 +18,11 @@ return require('packer').startup(function(use)
             }
         end
     })
+
     use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
     use('nvim-treesitter/playground')
     use('theprimeagen/harpoon')
+    use('github/copilot.vim')
     use('mbbill/undotree')
     use('tpope/vim-fugitive')
      use {
